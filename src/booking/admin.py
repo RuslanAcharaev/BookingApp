1631a0daa5395c68d71a30038a307eb5deb4f09b
Ruslan Acharaev <racharaev@gmail.com>
2025-11-11 13:52:41 +0300

    add admin panel

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Booking, Room
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    pass
